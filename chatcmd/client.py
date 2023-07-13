30b27d61291cd03cc5e768480ce2b9f796afca64
Gvard Windlass <gvard.windlass@gmail.com>
2023-07-13 14:36:03 +0300

    Rafactor client into a class

--- chatcmd/client.py
+++ chatcmd/client.py
@@ -12,25 +12,28 @@ from .utils import *
 from .store import MessageStore
 
 
-async def send_message(message: str, writer: StreamWriter):
-    writer.write((message + "\n").encode())
-    await writer.drain()
+class ChatClient:
+    def __init__(self) -> None:
+        self._writer: StreamWriter
+        self._reader: StreamReader
+        self._stdin_reader: StreamReader
+        self._messages: MessageStore
 
+    async def _send_message(self, message: str):
+        self._writer.write((message + "\n").encode())
+        await self._writer.drain()
 
-async def listen_for_messages(reader: StreamReader, message_store: MessageStore):  # A
-    while (message := await reader.readline()) != b"":
-        await message_store.append(message.decode())
-    await message_store.append("Server closed connection.")
+    async def _listen_for_messages(self):  # A
+        while (message := await self._reader.readline()) != b"":
+            await self._messages.append(message.decode())
+        await self._messages.append("Server closed connection.")
 
+    async def _read_and_send(self):  # B
+        while True:
+            message = await read_line(self._stdin_reader)
+            await self._send_message(message)
 
-async def read_and_send(stdin_reader: StreamReader, writer: StreamWriter):  # B
-    while True:
-        message = await read_line(stdin_reader)
-        await send_message(message, writer)
-
-
-async def main():
-    async def redraw_output(items: deque):
+    async def _redraw_output(self, items: deque):
         save_cursor_position()
         move_to_top_of_screen()
         for item in items:
@@ -38,48 +41,56 @@ async def main():
             sys.stdout.write(item)
         restore_cursor_position()
 
-    # switch terminal to raw mode to avoid race conditions
-    fd = sys.stdin.fileno()
-    old_settings = termios.tcgetattr(fd)
-    tty.setcbreak(fd)
+    async def start_chat_client(self):
+        # switch terminal to raw mode to avoid race conditions
+        fd = sys.stdin.fileno()
+        old_settings = termios.tcgetattr(fd)
+        tty.setcbreak(fd)
 
-    os.system("clear")
-    rows = move_to_bottom_of_screen()
+        os.system("clear")
+        rows = move_to_bottom_of_screen()
 
-    messages = MessageStore(redraw_output, rows - 1)
+        self._messages = MessageStore(self._redraw_output, rows - 1)
 
-    stdin_reader = await create_stdin_reader()
-    sys.stdout.write("Enter username: ")
-    sys.stdout.flush()
-    username = await read_line(stdin_reader)
-
-    try:
-        reader, writer = await asyncio.open_connection("127.0.0.1", 8000)  # C
-    except:
-        termios.tcsetattr(fd, termios.TCSADRAIN, old_settings)
-        sys.stdout.write("Could not connect to server\n")
-        return
-
-    writer.write(f"CONNECT {username}\n".encode())
-    await writer.drain()
-
-    message_listener = asyncio.create_task(listen_for_messages(reader, messages))  # D
-    input_listener = asyncio.create_task(read_and_send(stdin_reader, writer))
-
-    try:
-        _, pending = await asyncio.wait(
-            [message_listener, input_listener], return_when=asyncio.FIRST_COMPLETED
-        )
-        for task in pending:
-            task.cancel()
-    except Exception as e:
-        logging.exception(e)
-        writer.close()
-        await writer.wait_closed()
-    finally:
+        self._stdin_reader = await create_stdin_reader()
+        sys.stdout.write("Enter username: ")
         sys.stdout.flush()
-        # switch terminal back to echo mode
-        termios.tcsetattr(fd, termios.TCSADRAIN, old_settings)
+        username = await read_line(self._stdin_reader)
+
+        try:
+            self._reader, self._writer = await asyncio.open_connection(
+                "127.0.0.1", 8000
+            )  # C
+        except:
+            termios.tcsetattr(fd, termios.TCSADRAIN, old_settings)
+            sys.stdout.write("Could not connect to server\n")
+            return
+
+        self._writer.write(f"CONNECT {username}\n".encode())
+        await self._writer.drain()
+
+        message_listener = asyncio.create_task(self._listen_for_messages())  # D
+        input_listener = asyncio.create_task(self._read_and_send())
+
+        try:
+            _, pending = await asyncio.wait(
+                [message_listener, input_listener], return_when=asyncio.FIRST_COMPLETED
+            )
+            for task in pending:
+                task.cancel()
+        except Exception as e:
+            logging.exception(e)
+            self._writer.close()
+            await self._writer.wait_closed()
+        finally:
+            sys.stdout.flush()
+            # switch terminal back to echo mode
+            termios.tcsetattr(fd, termios.TCSADRAIN, old_settings)
+
+
+async def main():
+    chat_client = ChatClient()
+    await chat_client.start_chat_client()
 
 
 try:
